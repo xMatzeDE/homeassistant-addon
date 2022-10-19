b825f2ca10a6aa4f83c2344fdcbec67e58199443
Tim Weyand <tim.weyand@klambt.de>
2022-10-19 22:03:25 +0200

    initial commit
    Readme & Version bump
    
    change git tree

---
+++ comfoair2mqtt/README.md
@@ -0,0 +1,40 @@
+# Home Assistant Add-on: Comfoair 2 MQTT
+
+HAOS Addon Wrapper for [hacomfoairmqtt](https://github.com/adorobis/hacomfoairmqtt).
+
+![Supports aarch64 Architecture][aarch64-shield] ![Supports amd64 Architecture][amd64-shield] ![Supports armhf Architecture][armhf-shield] ![Supports armv7 Architecture][armv7-shield] ![Supports i386 Architecture][i386-shield]
+
+## What does this Addon do? 
+1. Expose [hacomfoairmqtt](https://github.com/adorobis/hacomfoairmqtt) Configuration in Homeassistant
+2. Working Python Environment
+3. Installation of [hacomfoairmqtt](https://github.com/adorobis/hacomfoairmqtt)
+
+## Installation in HomeAssistant OS
+1. If you don't have a MQTT broker yet; in Home Assistant go to Settings → Add-ons → Add-on store and install the Mosquitto broker addon.
+2. Go back to the Add-on store, click ⋮ → Repositories, fill in
+https://github.com/TimWeyand/homeassistant-addon and click Add → Check for Updates -> Close.
+3. The repository includes one add-on:
+   - **Comfoair 2 MQTT** is the latest tested release.
+4. Click on the addon and press **Install** and wait till the addon is installed.
+5. Click on Configuration
+   - Fill in the serial details (e.g. port of your USB Serial Converter). If you don't know the port and you have just one USB device connected to your machine try `/dev/ttyUSB0`. Else use the [Home Assistant CLI](https://www.home-assistant.io/common-tasks/os#home-assistant-via-the-command-line) and execute `ha hardware info` to find out. 
+   ```yaml
+        /dev/ttyUSB0
+   ```
+   - If you are **not** using the Mosquitto broker addon fill in your MQTT details (leave empty when using the Mosquitto broker addon). Format can be found [here](https://www.zigbee2mqtt.io/guide/configuration/mqtt.html#server-connection), but skip the initial `mqtt:` indent. e.g.: <br>
+        ```yaml
+        host: localhost
+        port: 1883
+        user: my_user
+        password: my_password
+        keepalive: 45
+        ```
+6. Start the addon by going to **Info** and click **Start**
+
+## Issues / Discussion Thread
+Discussions about hacomfoair can be found [here](https://community.home-assistant.io/t/zehnder-comfoair-ca350-integration-via-serial-connection-rs232-and-mqtt/173243/249)
+
+## Credits
+
+- adorobis for the main work @ [hacomfoairmqtt](https://github.com/adorobis/hacomfoairmqtt)
+
---
+++ comfoair2mqtt/docker/rootfs/docker-entrypoint.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bashio
+
+bashio::log.info "Comfoair2MQTT Addon"
+bashio::log.info "Preparing to start ... "
+
+# Check if HA supervisor started
+bashio::config.require 'serial'
+
+bashio::log.info "Generating Configuration 'config.ini' from HomeAssistant Addon Settings"
+
+# Expose addon configuration through environment variables.
+function export_config() {
+    local key=${1}
+    local subkey
+
+    if bashio::config.is_empty "${key}"; then
+        return
+    fi
+
+    for subkey in $(bashio::jq "$(bashio::config "${key}")" 'keys[]'); do
+        export "COMFOAIR2MQTT_CONFIG_$(bashio::string.upper "${key}")_$(bashio::string.upper "${subkey}")=$(bashio::config "${key}.${subkey}")"
+    done
+}
+
+export_config 'mqtt'
+
+if bashio::config.is_empty 'mqtt' && bashio::var.has_value "$(bashio::services 'mqtt')"; then
+    export COMFOAIR2MQTT_CONFIG_MQTT_HOST=$(bashio::services 'mqtt' 'host')
+    export COMFOAIR2MQTT_CONFIG_MQTT_PORT=$(bashio::services 'mqtt' 'port')
+    export COMFOAIR2MQTT_CONFIG_MQTT_USER="$(bashio::services 'mqtt' 'username')"
+    export COMFOAIR2MQTT_CONFIG_MQTT_PASSWORD="$(bashio::services 'mqtt' 'password')"
+    export COMFOAIR2MQTT_CONFIG_MQTT_KEEPALIVE="45"
+fi
+
+if bashio::var.true $(bashio::config 'RS485_protocol'); then
+  export COMFOAIR2MQTT_CONFIG_RS485="True";
+else
+  export COMFOAIR2MQTT_CONFIG_RS485="False";
+fi
+
+if bashio::var.true $(bashio::config 'enablePcMode'); then
+  export COMFOAIR2MQTT_CONFIG_PCMode="True";
+else
+  export COMFOAIR2MQTT_CONFIG_PCMode="False";
+fi
+
+if bashio::var.true $(bashio::config 'debug'); then
+  export COMFOAIR2MQTT_CONFIG_debug="True";
+else
+  export COMFOAIR2MQTT_CONFIG_debug="False";
+fi
+
+if bashio::var.true $(bashio::config 'HAEnableAutoDiscoverySensors'); then
+  export COMFOAIR2MQTT_CONFIG_DiscoverySensor="True";
+else
+  export COMFOAIR2MQTT_CONFIG_DiscoverySensor="False";
+fi
+
+if bashio::var.true $(bashio::config 'HAEnableAutoDiscoveryClimate'); then
+  export COMFOAIR2MQTT_CONFIG_DiscoveryClimate="True";
+else
+  export COMFOAIR2MQTT_CONFIG_DiscoveryClimate="False";
+fi
+
+
+cat <<EOF > "/opt/hacomfoairmqtt/src/config.ini"
+[DEFAULT]
+SerialPort=$(bashio::config 'serial')
+RS485_protocol=$COMFOAIR2MQTT_CONFIG_RS485
+refresh_interval=$(bashio::config 'refresh_interval')
+enablePcMode=$COMFOAIR2MQTT_CONFIG_PCMode
+debug=$COMFOAIR2MQTT_CONFIG_debug
+
+[MQTT]
+MQTTServer=$COMFOAIR2MQTT_CONFIG_MQTT_HOST
+MQTTPort=$COMFOAIR2MQTT_CONFIG_MQTT_PORT
+MQTTUser=$COMFOAIR2MQTT_CONFIG_MQTT_USER
+MQTTPassword=$COMFOAIR2MQTT_CONFIG_MQTT_PASSWORD
+MQTTKeepalive=$COMFOAIR2MQTT_CONFIG_MQTT_KEEPALIVE
+
+[HA]
+HAEnableAutoDiscoverySensors=$COMFOAIR2MQTT_CONFIG_DiscoverySensor
+HAEnableAutoDiscoveryClimate=$COMFOAIR2MQTT_CONFIG_DiscoveryClimate
+HAAutoDiscoveryDeviceId=$(bashio::config 'HAAutoDiscoveryDeviceId')
+HAAutoDiscoveryDeviceName=$(bashio::config 'HAAutoDiscoveryDeviceName')
+HAAutoDiscoveryDeviceManufacturer=$(bashio::config 'HAAutoDiscoveryDeviceManufacturer')
+HAAutoDiscoveryDeviceModel=$(bashio::config 'HAAutoDiscoveryDeviceModel')
+EOF
+
+bashio::log.info "Startup hacomfoairmqtt"
+python3 /opt/hacomfoairmqtt/src/ca350
